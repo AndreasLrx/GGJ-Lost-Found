17943750c579e25d836738903aad1ef8c40ecff7
ZeBoss <mathias.blnchard@epitech.eu>
2021-01-31 01:13:54 +0100

    [ADD] Now player can change from map to map with arrrow, all debug is remove EXCEPT Line 62 in Floor.cpp, also create a mockup mevem

--- srcs/Map/Floor.cpp
+++ srcs/Map/Floor.cpp
@@ -32,29 +32,78 @@ Floor::Floor(GameDataRef data, Alien *alien)
     this->m_data = data;
     this->m_alien = alien;
     this->m_rooms = nullptr;
+    this->floor_pos = {0, 0};
     this->set(std::string("1111111"), data);
 }
 
 void Floor::change_room(float dt, std::string cur_room)
 {
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::J)) {
-        this->m_cur_room++;
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && floor_pos.y - 1 >= 0)
+        if (this->floor_map[floor_pos.y - 1][floor_pos.x] != '.') {
+            floor_pos.y--;
+            this->m_cur_room -= floor_map.size();
+        }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && floor_pos.y + 1 < (int)floor_map.size()) {
+        if (this->floor_map[floor_pos.y + 1][floor_pos.x] != '.') {
+            this->m_cur_room += floor_map.size();
+            this->floor_pos.y++;
+        }
     }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && floor_pos.x -1 >= 0)
+        if (this->floor_map[floor_pos.y][floor_pos.x - 1] != '.'){
+            this->m_cur_room--;
+            floor_pos.x--;
+        }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && floor_pos.x + 1 < (int)floor_map[floor_pos.y].size())
+        if (this->floor_map[floor_pos.y][floor_pos.x +1] != '.') {
+            this->m_cur_room++;
+            floor_pos.x++;
+        }
+/*    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
+        for (int i = 0; i < (int)this->floor_map.size(); i++) {
+            std::cout << floor_map[i] << " :  " << &m_rooms[i * 3 + 0] << ",  " << &m_rooms[i * 3 + 1] << ",  " << &m_rooms[i * 3 + 2] <<'\n';
+        }
+        std::cout << floor_pos.x << '.' << floor_pos.y << " :  " << this->m_cur_room << "\n\n\n";
+    }*/
     m_cur_room->update(dt);
+}
+
+std::vector<std::string> floor_map_init(std::string str)
+{
+    std::stringstream str_buffer = (std::stringstream)str;
+    std::string segment;
+    std::vector<std::string> seglist;
+
+    while(std::getline(str_buffer, segment))
+        seglist.push_back(segment);
+    return (seglist);
 }
 
 void Floor::set(std::string cur_floor, GameDataRef data)
 {
-    std::ifstream read_floor((std::string)"Level/Floor/floor_" + "1");
+    std::ifstream read_floor((std::string)"Level/Floor/floor_" + "1"); // Load the floor (level) you want. WIP
     std::stringstream floor_buffer;
-    floor_buffer << read_floor.rdbuf();
-    this->m_rooms = new Room[floor_buffer.str().size()];
-    for (unsigned int i = 0; i < floor_buffer.str().size(); i++) {
-        std::ifstream read_room((std::string)"Level/Room/basic" + floor_buffer.str()[i]);
-        std::stringstream room_buffer;
-        room_buffer << read_room.rdbuf();
-        this->m_rooms[i].setAlien(m_alien);
-        this->m_rooms[i].set(room_buffer.str(), data);
+    int size = 0; // number of room.
+    int i = 0; // index to naviguate through room;
+    floor_buffer << read_floor.rdbuf();  // idk
+    this->floor_map = floor_map_init(floor_buffer.str()); // init string vector.
+    for (unsigned long i = 0; i < floor_map.size(); i++) // compute size
+        size += floor_map[i].size();
+    this->m_rooms = new Room[size]; // Allocate room
+    this->m_floor_str = floor_buffer.str(); // Useless i think ?
+    for (int index = 0; index < size; index++) { // For each room in the floor
+        if (floor_buffer.str()[index] != '\n') { // If index of floor buffer is  a room
+            if (floor_buffer.str()[index] == '.') {
+                i++;
+        } else {
+                std::ifstream read_room((std::string)"Level/Room/basic" + floor_buffer.str()[index]); // Load good room
+                std::stringstream room_buffer;
+                room_buffer << read_room.rdbuf();
+                this->m_rooms[i].setAlien(m_alien);
+                this->m_rooms[i].set(room_buffer.str(), data);
+                i++;
+            }
+        }
     }
     this->m_cur_room = &m_rooms[0];
 }
