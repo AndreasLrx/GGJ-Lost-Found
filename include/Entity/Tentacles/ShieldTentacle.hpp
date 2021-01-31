77697a7be84dc1215e1a21d737b3660a74249c8e
Yanis Guaye <yanis.guaye@epitech.eu>
2021-01-31 08:26:33 +0100

    Add Shield tantacles

---
+++ include/Entity/Tentacles/ShieldTentacle.hpp
@@ -0,0 +1,21 @@
+﻿/*
+** EPITECH PROJECT, 2021
+** GGJ Alien
+** File description:
+** An Alien's shield tentacle
+*/
+
+#ifndef SHIELD_TENTACLE_HPP
+#define SHIELD_TENTACLE_HPP
+
+#include "Entity/Tentacles/Tentacle.hpp"
+
+class Alien;
+
+class ShieldTentacle : public Tentacle
+{
+public:
+    ShieldTentacle();
+};
+
+#endif // !defined(SHIELD_TENTACLE_HPP)
